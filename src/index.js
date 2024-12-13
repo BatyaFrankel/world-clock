04ad80c3a2c95c3539ffc2dfa4deba7b45e19df4
BatyaFrankel <frankelbatya@gmail.com>
2024-12-13 10:15:58 +0000

    created home page js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,34 @@
-let londonTimeElement = document.querySelector("#displayed-citys");
-let londonTime = moment().tz("Europe/London");
-londonTimeElement.innerHTML = `<div class="city">
-            <h2 class="city-timezone" id="london">London</h2>
-            <div class="time">${londonTime.format(
-              `h:m:ss`
-            )} <span>${londonTime.format(`A`)}</span></div>
-            <div class="date">${londonTime.format(`dddd, MMMM D, YYYY`)}</div>
-          </div>`;
+function updateTime() {
+  let displayedCitysElement = document.querySelector("#displayed-citys");
 
-console.log(londonTimeElement);
+  const cities = [
+    { name: "London", timezone: "Europe/London" },
+    { name: "Sydney", timezone: "Australia/Sydney" },
+    { name: "Tokyo", timezone: "Asia/Tokyo" },
+    { name: "New York", timezone: "America/New_York" },
+  ];
+
+  displayedCitysElement.innerHTML = "";
+  cities.forEach(function (city) {
+    let cityTime = moment().tz(city.timezone);
+    displayedCitysElement.innerHTML += `
+      <div class="city">
+        <h2 class="city-timezone">${city.name}</h2>
+        <div class="time">${cityTime.format("h:mm:ss")} <span>${cityTime.format(
+      "A"
+    )}</span></div>
+        <div class="date">${cityTime.format("dddd, D MMMM YYYY")}</div>
+      </div>
+    `;
+  });
+}
+
+
+
+updateTime();
+setInterval(updateTime, 1000);
+/*function displayWorldClock(event){
+  if (event.target.value.length)
+}
+WorldClockElement = document.querySelector("#world-clock");
+WorldClockElement.addEventListerner(Change, displayWorldClock);
